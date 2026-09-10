0ecd2b9d48e28ecf6770d513c16294b4dd1dc256
Ubuntu <ubuntu@ip-172-31-8-190.ap-south-1.compute.internal>
2026-09-10 07:32:44 +0000

    week4 day7: compose.prod terraform ansible

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "web" {
+  name        = "week4-web-sg"
+  description = "ssh from lab, http world"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  associate_public_ip_address = true
+  tags = { Name = "week4-app" }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region"    { default = "ap-south-1" }
+variable "key_name"      { default = "week4-ansible" }
+variable "ssh_cidr"      { default = "13.201.167.23/32" }
+variable "instance_type" { default = "t3.small" }
